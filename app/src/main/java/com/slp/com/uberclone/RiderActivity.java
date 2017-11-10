875e94c9ed3fe75b636e8bfff12d03cccb90d857
Lakshmiprasad <slakshmiprasad93@gmail.com>
2017-11-11 01:00:21 +0530

    feat: ride button + theme

--- app/src/main/java/com/slp/com/uberclone/RiderActivity.java
+++ app/src/main/java/com/slp/com/uberclone/RiderActivity.java
@@ -9,10 +9,12 @@ import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -180,5 +182,9 @@ public class RiderActivity extends FragmentActivity implements OnMapReadyCallbac
             buildGoogleApi();
             accessLocation();
         }
+    }
+
+    public void bookRide(View view) {
+        Snackbar.make(view,"Ubering!",Snackbar.LENGTH_SHORT).show();
     }
 }
